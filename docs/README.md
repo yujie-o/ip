71f4059bbc7d9b924e1c0a480d024a40907ac840
yujie-o <yujie2164@gmail.com>
2025-08-22 16:19:31 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Penguin User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Penguin.java
@@ -0,0 +1,12 @@
+public class Penguin {
+    public static void main(String[] args) {
+        String name = "Penguin";
+        String line = ("____________________________________________________________");
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
