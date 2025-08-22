ac017294d56ba63466002942ac0d95283fe7616b
yujie-o <yujie2164@gmail.com>
2025-08-22 23:15:13 +0800

    Rename, Greet, Exit

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,12 +1,14 @@
 public class Penguin {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String name = "Penguin";
-        String line = ("____________________________________________________________");
-        System.out.println(line);
-        System.out.println("Hello! I'm " + name);
+        String chatbotName = "Penguin";
+
+        System.out.println(LINE);
+        System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
     }
 }
